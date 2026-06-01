ea4495fe7ec9baee1c2a157288a96165816866e7
Marley Marques <seu_email@gmail.com>
2026-06-01 16:00:11 -0300

    chore: cria estrutura inicial do projeto spring boot

---
+++ src/test/java/br/com/marley/escala/EscalaPlantoesApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.marley.escala;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EscalaPlantoesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
